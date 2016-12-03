00031370b753e5c47bd8e14f6c911324d1f54daa
iJungleboy <daniel.mettler@2sic.com>
2016-12-03 09:15:32 +0100

    made $2sxc lighter in read-only mode - part of https://github.com/2sic/2sxc/issues/1045

--- Js/2sxc.api.js
+++ Js/2sxc.api.js
@@ -253,7 +253,7 @@
     };
 
     $2sxc._controllers = {};
-    $2sxc.name = "The 2sxc Controller object";
+    $2sxc.description = "The 2sxc Controller object";
     // $2sxc.version = "08.07.00";   // todo: get from url param
     $2sxc.beta = {};
     $2sxc._data = {};
@@ -333,22 +333,6 @@
         }
     };
 
-    // debug state which is needed in various places
-    $2sxc.debug = {
-        load: ($2sxc.urlParams.get("debug") === "true"),
-        uncache: $2sxc.urlParams.get("sxcver")
-    };
-
-    // mini-helpers to manage 2sxc parts, a bit like a dependency loader which will optimize to load min/max depending on debug state
-    $2sxc.parts = {
-        getUrl: function improveUrl(url, preventUnmin) {
-            var r = (preventUnmin || !$2sxc.debug.load) ? url : url.replace(".min", ""); // use min or not
-            if ($2sxc.debug.uncache && r.indexOf("sxcver") === -1)
-                r = r + ((r.indexOf("?") === -1) ? "?" : "&") + "sxcver=" + $2sxc.debug.uncache;
-            return r;
-        }
-    };
-
     // will find the controller based on the dom-element
     $2sxc.autoFind = function(domElement) {
         var containerTag = $(domElement).closest(".sc-content-block")[0];
@@ -357,22 +341,4 @@
         if (!iid || !cbid) return null;
         return $2sxc(iid, cbid);
     };
-
-    //#region System Commands - at the moment only finishUpgrade
-    $2sxc.system = {
-        // upgrade command - started when an error contains a link to start this
-        finishUpgrade: function(domElement) {
-            var mc = $2sxc(domElement);
-            $.ajax({
-                type: "get",
-                url: mc.resolveServiceUrl("view/module/finishinstallation"),
-                beforeSend: $.ServicesFramework(mc.id).setModuleHeaders
-            }).success(function() {
-                alert("Upgrade ok, restarting the CMS and reloading...");
-                location.reload();
-            });
-            alert("starting upgrade. This could take a few minutes. You'll see an 'ok' when it's done. Please wait...");
-        }
-    };
-    //#endregion
 })();
---
+++ src/inpage/2sxc-extensions/2sxc.api.edit-extensions.js
@@ -0,0 +1,41 @@
+﻿// this enhances the $2sxc client controller with stuff only needed when logged in
+(function() {
+    if ($2sxc) {
+
+    	// debug state which is needed in various places
+    	$2sxc.debug = {
+    		load: ($2sxc.urlParams.get("debug") === "true"),
+    		uncache: $2sxc.urlParams.get("sxcver")
+    	};
+
+
+    	// mini-helpers to manage 2sxc parts, a bit like a dependency loader which will optimize to load min/max depending on debug state
+    	$2sxc.parts = {
+    		getUrl: function improveUrl(url, preventUnmin) {
+    			var r = (preventUnmin || !$2sxc.debug.load) ? url : url.replace(".min", ""); // use min or not
+    			if ($2sxc.debug.uncache && r.indexOf("sxcver") === -1)
+    				r = r + ((r.indexOf("?") === -1) ? "?" : "&") + "sxcver=" + $2sxc.debug.uncache;
+    			return r;
+    		}
+    	};
+
+        //#region System Commands - at the moment only finishUpgrade
+        $2sxc.system = {
+            // upgrade command - started when an error contains a link to start this
+            finishUpgrade: function(domElement) {
+                var mc = $2sxc(domElement);
+                $.ajax({
+                    type: "get",
+                    url: mc.resolveServiceUrl("view/module/finishinstallation"),
+                    beforeSend: $.ServicesFramework(mc.id).setModuleHeaders
+                }).success(function() {
+                    alert("Upgrade ok, restarting the CMS and reloading...");
+                    location.reload();
+                });
+                alert("starting upgrade. This could take a few minutes. You'll see an 'ok' when it's done. Please wait...");
+            }
+        };
+        //#endregion
+
+    }
+})();
