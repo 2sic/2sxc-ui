5fadf5d9680e69ae8ad89f1165aed8c853035c30
iJungleboy <daniel.mettler@2sic.com>
2017-08-27 00:59:32 +0200

    rebuild all js, ready for new release

--- dist/sxc-admin/sxc-admin.js
+++ dist/sxc-admin/sxc-admin.js
@@ -1198,24 +1198,24 @@ angular.module("SxcAdminUi", [
         function openZoneMain(zoneId, closeCallback) {
             var resolve = eavAdminDialogs.CreateResolve({ zoneId: zoneId });
             return eavAdminDialogs.OpenModal("apps-management/apps.html", "AppList as vm", "xlg", resolve, closeCallback);
-        };
+        }
 
         // the app-level dialog showing all app content-items, templates, web-api etc.
         function openAppMain(appId, closeCallback) {
             var resolve = eavAdminDialogs.CreateResolve({ appId: appId });
             return eavAdminDialogs.OpenModal("app-main/app-main.html", "AppMain as vm", "xlg", resolve, closeCallback);
-        };
+        }
 
         // the app-level dialog showing all app content-items, templates, web-api etc.
         function openAppImport(closeCallback) {
             var resolve = eavAdminDialogs.CreateResolve({}); // { appId: appId }});
             return eavAdminDialogs.OpenModal("importexport/import-app.html", "ImportApp as vm", "lg", resolve, closeCallback);
-        };
+        }
 
         //#region Total-Popup open / close
         function openTotal(url, callback) {
             return $2sxc.totalPopup.open(service.browserFixUrlCaching(url), callback);
-        };
+        }
 
         function browserFixUrlCaching(url) {
             // this fixes a caching issue on IE and FF - see https://github.com/2sic/2sxc/issues/444
@@ -1224,40 +1224,34 @@ angular.module("SxcAdminUi", [
             if (url.match(urlCheck))
                 url = url.replace(urlCheck, "$1&time=" + new Date().getTime());
             return url;
-        };
+        }
 
         function closeThis() {
             return $2sxc.totalPopup.closeThis();
-        };
+        }
         //#endregion
 
         // the replace-content item
         function openReplaceContent(item, closeCallback) {
             var resolve = eavAdminDialogs.CreateResolve({ item: item });
             return eavAdminDialogs.OpenModal("replace-content/replace-content.html", "ReplaceDialog as vm", "lg", resolve, closeCallback);
-        };
+        }
 
         function openManageContentList(item, closeCallback) {
             var resolve = eavAdminDialogs.CreateResolve({ item: item });
             return eavAdminDialogs.OpenModal("manage-content-list/manage-content-list.html", "ManageContentList as vm", "", resolve, closeCallback);
-        };
+        }
 
         function openDevelop(item, closeCallback) {
             eavAdminDialogs.openModalComponent("editor", "max", { item: item }, closeCallback);
             //var resolve = eavAdminDialogs.CreateResolve({ item: item });
             //return eavAdminDialogs.OpenModal("source-editor/editor.html", "Editor as vm", "max", resolve, closeCallback);
-        };
-
-        // 2dm 2015-10-07 - don't think this is in use, remove
-        //svc.openContentEdit = function oce(edit, closeCallback) {
-        //    var resolve = eavAdminDialogs.CreateResolve(edit);
-        //    return eavAdminDialogs.OpenModal("wrappers/dnn-wrapper.html", "EditInDnn as vm", "lg", resolve, closeCallback);
-        //};
+        }
 
         function openLanguages(zoneId, closeCallback) {
             var resolve = eavAdminDialogs.CreateResolve({ zoneId: zoneId });
             return eavAdminDialogs.OpenModal("language-settings/languages.html", "LanguageSettings as vm", "lg", resolve, closeCallback);
-        };
+        }
     }]);
 angular.module("SxcServices")
     /*@ngInject*/
