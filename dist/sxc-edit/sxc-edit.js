e274e32e3998b132fbc4b0a50074702c882bd755
Tonći Vatavuk <tonci@sistemi.hr>
2018-04-11 07:01:12 +0200

    fix Edge issue

--- dist/sxc-edit/sxc-edit.js
+++ dist/sxc-edit/sxc-edit.js
@@ -387,13 +387,21 @@ angular.module('Adam')
                     // if it is a image form clipboard
                     var img = clipData.items[key].getAsFile();
 
-                    var imageFileName = 'image.png';
+                    // input image filename
+                    if (browser() !== 'Edge') {
 
-                    imageFileName = window.prompt('Enter clipboard image file name: ', imageFileName); // todo i18n 
+                        var imageFileName = 'image.png';
 
-                    var imgFile = twoSxcFile(img, imageFileName);
+                        // todo: generate has sha256 for name
 
-                    vm.dropzone.dz.processFile(imgFile);
+                        imageFileName = window.prompt('Enter clipboard image file name: ', imageFileName); // todo i18n 
+
+                        img = twoSxcFile(img, imageFileName);
+                    }
+
+                    // todo: convert png to jpg
+
+                    vm.dropzone.dz.processFile(img);
 
                 };
             });
@@ -410,6 +418,52 @@ angular.module('Adam')
             var newFile = data.get('file');
             return newFile;
         };
+
+        /**
+         * Gets the browser name or returns an empty string if unknown. 
+         * This function also caches the result to provide for any 
+         * future calls this function has.
+         * https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+         *
+         * @returns {string}
+         */
+        function browser() {
+            // Return cached result if avalible, else get result then cache it.
+            if (browser.prototype._cachedResult)
+                return browser.prototype._cachedResult;
+
+            // Opera 8.0+
+            var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+            // Firefox 1.0+
+            var isFirefox = typeof InstallTrigger !== 'undefined';
+
+            // Safari 3.0+ "[object HTMLElementConstructor]" 
+            var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+            // Internet Explorer 6-11
+            var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+            // Edge 20+
+            var isEdge = !isIE && !!window.StyleMedia;
+
+            // Chrome 1+
+            var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+            // Blink engine detection
+            var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+            return browser.prototype._cachedResult =
+                isOpera ? 'Opera' :
+                isFirefox ? 'Firefox' :
+                isSafari ? 'Safari' :
+                isChrome ? 'Chrome' :
+                isIE ? 'IE' :
+                isEdge ? 'Edge' :
+                isBlink ? 'Blink' :
+                "Don't know";
+        };
+
     }
 
 })();
