1d5dd61c3f60a550daa523cdd741d23d08052c7f
Tonći Vatavuk <tonci@sistemi.hr>
2018-04-12 08:37:36 +0200

    wip: paste in tiny and adam input

--- src/sxc-edit/adam/dropzone.directive.js
+++ src/sxc-edit/adam/dropzone.directive.js
@@ -1,5 +1,5 @@
 ﻿/* js/fileAppDirectives */
-(function() {
+(function () {
     angular.module('Adam')
         /*@ngInject*/
         .directive('dropzone', function (sxc, tabId, AppInstanceId, ContentBlockId, dragClass, adamSvc, $timeout, $translate) {
@@ -88,17 +88,138 @@
                 $timeout(function () {
                     var dropzone = new Dropzone(element[0], config);
 
-                    angular.forEach(eventHandlers, function(handler, event) {
+                    angular.forEach(eventHandlers, function (handler, event) {
                         dropzone.on(event, handler);
                     });
 
-                    scope.processDropzone = function() { dropzone.processQueue(); };
-                    scope.resetDropzone = function() { dropzone.removeAllFiles(); };
+                    scope.processDropzone = function () { dropzone.processQueue(); };
+                    scope.resetDropzone = function () { dropzone.removeAllFiles(); };
                     controller.openUpload = function () { dropzone.hiddenFileInput.click(); };
                     controller.processFile = function (file) { dropzone.processFile(file); }; // needed for clipboard paste
 
+
+                    var pasteInstance;
+
+                    // pastableTextarea - for adam input
+                    pasteInstance = $(element[0]).children("div:first").children("div.after-preview:first").children("div:first").children("input:first");
+                    if (pasteInstance.length > 0) {
+                        pasteInstance.pastableTextarea();
+                        pasteInstance.on('pasteImage', function (ev, data) {
+                            pasteImageInDropzone(ev, data, dropzone);
+                        });
+
+                        // pastableNonInputable
+                        //pasteInstance = $(element[0]);
+                        //if (pasteInstance.length > 0) {
+                        //    pasteInstance.pastableNonInputable();
+                        //    pasteInstance.on('pasteImage', function (ev, data) {
+                        //        pasteImageInDropzone(ev, data, dropzone);
+                        //    });
+                        //}
+
+                    }
+
+                    // pastableContenteditable - for tinymce
+                    pasteInstance = $(element[0]).children("div:first").children("div[contenteditable]:first");
+                    if (pasteInstance.length > 0) {
+                        pasteInstance.pastableContenteditable();
+                        pasteInstance.on('pasteImage', function (ev, data) {
+                            pasteImageInDropzone(ev, data, dropzone);
+                        });
+                    }
+
+                    
+
+
+                    //.on('pasteText', function (ev, data) {
+                    //    debugger;
+                    //    //$('<div class="result"></div>').text('text: "' + data.text + '"').insertAfter(this);
+                    //}).on('pasteTextRich', function (ev, data) {
+                    //    debugger;
+                    //    //$('<div class="result"></div>').text('rtf: "' + data.text + '"').insertAfter(this);
+                    //}).on('pasteTextHtml', function (ev, data) {
+                    //    debugger;
+                    //    //$('<div class="result"></div>').text('html: "' + data.text + '"').insertAfter(this);
+                    //});
+
+
                 }, 0);
+
+                function pasteImageInDropzone(ev, data, dropzone) {
+                    var img = data.file;
+                    // input image filename
+                    if (browser() !== 'Edge') {
+                        var imageFileName = 'image.png';
+                        // todo: generate hash sha256 for name
+                        imageFileName = window.prompt('Enter clipboard image file name: ', imageFileName); // todo: i18n 
+                        img = twoSxcFile(img, imageFileName);
+                    }
+                    // todo: convert png to jpg
+                    dropzone.processFile(img);
+                }
+
+                /**
+                 * creates new customized file
+                 * @param {File} file
+                 * @param {string} fileName
+                 */
+                function twoSxcFile(file, fileName) {
+                    var data = new FormData();
+                    data.append('file', file, fileName);
+                    var newFile = data.get('file');
+                    return newFile;
+                }
+
+                /**
+                 * Gets the browser name or returns an empty string if unknown. 
+                 * This function also caches the result to provide for any 
+                 * future calls this function has.
+                 * https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+                 *
+                 * @returns {string}
+                 */
+                function browser() {
+                    // Return cached result if avalible, else get result then cache it.
+                    if (browser.prototype._cachedResult)
+                        return browser.prototype._cachedResult;
+
+                    // Opera 8.0+
+                    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+                    // Firefox 1.0+
+                    var isFirefox = typeof InstallTrigger !== 'undefined';
+
+                    // Safari 3.0+ "[object HTMLElementConstructor]" 
+                    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window.safari || safari.pushNotification);
+
+                    // Internet Explorer 6-11
+                    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+                    // Edge 20+
+                    var isEdge = !isIE && !!window.StyleMedia;
+
+                    // Chrome 1+
+                    var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+                    // Blink engine detection
+                    var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+                    browser.prototype._cachedResult =
+                        isOpera ? 'Opera' :
+                            isFirefox ? 'Firefox' :
+                                isSafari ? 'Safari' :
+                                    isChrome ? 'Chrome' :
+                                        isIE ? 'IE' :
+                                            isEdge ? 'Edge' :
+                                                isBlink ? 'Blink' :
+                                                    "Don't know";
+
+                    return browser.prototype._cachedResult;
+                }
+
             }
+
+
 
             /*@ngInject*/
             function controller() {
