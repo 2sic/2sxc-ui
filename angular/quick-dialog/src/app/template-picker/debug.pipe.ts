dd5b6f45f064f567bd638070fcc5f062e8ce87be
iJungleboy <daniel.mettler@2sic.com>
2018-10-22 15:15:50 +0200

    wip, not stable yet

--- angular/quick-dialog/src/app/template-picker/debug.pipe.ts
+++ angular/quick-dialog/src/app/template-picker/debug.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'debug', pure: true })
 export class DebugPipe implements PipeTransform {
     transform<T>(obj: T, note: string): T {
-        console.log(`pd:${note}`, obj);
+        console.log(`pd:${note} (${typeof(obj)})`, obj);
         return obj;
     }
 }
