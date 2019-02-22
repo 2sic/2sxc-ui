bd9973d44de79c2c836c4d402bba28bf7ede645d
Tonći Vatavuk <tonci@sistemi.hr>
2019-02-22 13:36:49 +0100

    related to https://github.com/2sic/2sxc/issues/1657 file manager ignores file access rights and root path

--- src/sxc-edit/adam/adam.service.js
+++ src/sxc-edit/adam/adam.service.js
@@ -1,140 +1,167 @@
 // note: don't prefix angular with window - something fails in production build if you do that
 // ReSharper disable PossiblyUnassignedProperty
 angular.module('Adam')
-    /*@ngInject*/
-    .factory('adamSvc', function ($http, eavConfig, sxc, svcCreator, appRoot, appId) {
+  /*@ngInject*/
+  .factory('adamSvc', function ($http, eavConfig, sxc, svcCreator, appRoot, appId, sanitizeSvc, $filter) {
 
-        // Construct a service for this specific appId
-        return function createSvc(contentType, entityGuid, field, subfolder, serviceConfig) {
-            var svc = {
-                url: sxc.resolveServiceUrl('app-content/' + contentType + '/' + entityGuid + '/' + field),
-                subfolder: subfolder,
-                folders: [],
-                adamRoot: appRoot.substr(0, appRoot.indexOf('2sxc'))
-            };
+    // Construct a service for this specific appId
+    return function createSvc(contentType, entityGuid, field, subfolder, serviceConfig) {
 
-            // get the correct url for uploading as it is needed by external services (dropzone)
-            svc.uploadUrl = function(targetSubfolder) {
-                var url = (targetSubfolder === '')
-                    ? svc.url
-                    : svc.url + '?subfolder=' + targetSubfolder;
-                url += (url.indexOf('?') === -1 ? '?' : '&')
-                    + 'usePortalRoot=' + serviceConfig.usePortalRoot
-                    + '&appId=' + appId;
-                return url;
-            };
+      subfolder = sanitizeSvc.sanitizePath(subfolder);
 
-            // extend a json-response with a path (based on the adam-root) to also have a fullPath
-            svc.addFullPath = function(value, key) {
-              // 2dm 2018-03-29 special fix - sometimes the path already has the full path, sometimes not
-              // it should actually be resolved properly, but because I don't have time
-              // ATM (data comes from different web-services, which are also used in other places
-              // I'll just check if it's already in there
-              value.fullPath = value.Path;
+      allowEdit = false;
 
-              if(value.Path && value.Path.toLowerCase().indexOf(svc.adamRoot.toLowerCase()) === -1)
-                value.fullPath = svc.adamRoot + value.Path;
-            };
+      var svc = {
+        url: sxc.resolveServiceUrl('app-content/' + contentType + '/' + entityGuid + '/' + field),
+        subfolder: subfolder,
+        folders: [],
+        adamRoot: appRoot.substr(0, appRoot.indexOf('2sxc')),
 
-            svc = angular.extend(svc, svcCreator.implementLiveList(function getAll() {
-                return $http.get(svc.url + '/items',
-                        {
-                            params: {
-                                subfolder: svc.subfolder,
-                                usePortalRoot: serviceConfig.usePortalRoot,
-                                appId: appId
-                            }
-                        })
-                    .then(function(result) {
-                        angular.forEach(result.data, svc.addFullPath);
-                        return result;
-                    });
-            }));
+      };
 
-            // create folder
-            svc.addFolder = function (newfolder) {
-                return $http.post(svc.url + '/folder',
-                        {},
-                        {
-                            params: {
-                                subfolder: svc.subfolder,
-                                newFolder: newfolder,
-                                usePortalRoot: serviceConfig.usePortalRoot,
-                                appId: appId
-                            }
-                        })
-                    .then(svc.liveListReload);
-            };
+      checkAllowEdit = function (items) {
+        console.log('stv items', items);
+        var currentFolder = $filter('filter')(items, { Name: '.' }, true)[0];
+        if (currentFolder) {
+          console.log('stv currentFolder', currentFolder);
+          allowEdit = currentFolder.AllowEdit;
+          // return currentFolder.AllowEdit;
+        }
+        else {
+          console.log('stv currentFolder missing');
+          allowEdit = false;
+          // return false;
+        }
+      };
 
-            svc.goIntoFolder = function(childFolder) {
-                svc.folders.push(childFolder);
-                var pathParts = childFolder.Path.split('/');
-                var subPath = '';
-                for (var c = 0; c < svc.folders.length; c++)
-                    subPath = pathParts[pathParts.length - c - 2] + '/' + subPath;
+      svc.getAllowEdit = function() {
+        return allowEdit;
+      };
 
-                subPath = subPath.replace('//', '/');
-                if (subPath[subPath.length - 1] === '/')
-                    subPath = subPath.substr(0, subPath.length - 1);
+      // get the correct url for uploading as it is needed by external services (dropzone)
+      svc.uploadUrl = function (targetSubfolder) {
+        targetSubfolder = sanitizeSvc.sanitizePath(targetSubfolder);
+        var url = (targetSubfolder === '')
+          ? svc.url
+          : svc.url + '?subfolder=' + targetSubfolder;
+        url += (url.indexOf('?') === -1 ? '?' : '&')
+          + 'usePortalRoot=' + serviceConfig.usePortalRoot
+          + '&appId=' + appId;
+        return url;
+      };
 
-                // add configured Paths
-                subPath = (!!subfolder) ? subfolder + '/' + subPath : subPath;
+      // extend a json-response with a path (based on the adam-root) to also have a fullPath
+      svc.addFullPath = function (value, key) {
+        // 2dm 2018-03-29 special fix - sometimes the path already has the full path, sometimes not
+        // it should actually be resolved properly, but because I don't have time
+        // ATM (data comes from different web-services, which are also used in other places
+        // I'll just check if it's already in there
+        value.fullPath = value.Path;
 
-                childFolder.Subfolder = subPath;
+        if (value.Path && value.Path.toLowerCase().indexOf(svc.adamRoot.toLowerCase()) === -1)
+          value.fullPath = svc.adamRoot + value.Path;
+      };
 
-                // now assemble the correct subfolder based on the folders-array
-                svc.subfolder = subPath;
-                svc.liveListReload();
-                return subPath;
-            };
+      svc = angular.extend(svc, svcCreator.implementLiveList(function getAll() {
+        return $http.get(svc.url + '/items',
+          {
+            params: {
+              subfolder: svc.subfolder,
+              usePortalRoot: serviceConfig.usePortalRoot,
+              appId: appId
+            }
+          })
+          .then(function (result) {
+            angular.forEach(result.data, svc.addFullPath);
+            checkAllowEdit(result.data);
+            return result;
+          });
+      }));
 
-            svc.goUp = function() {
-                if (svc.folders.length > 0)
-                    svc.folders.pop();
-                if (svc.folders.length > 0) {
-                    svc.subfolder = svc.folders[svc.folders.length - 1].Subfolder;
-                } else {
-                    svc.subfolder = subfolder || '';
-                }
-                svc.liveListReload();
-                return svc.subfolder;
-            };
+      // create folder
+      svc.addFolder = function (newfolder) {
+        return $http.post(svc.url + '/folder',
+          {},
+          {
+            params: {
+              subfolder: svc.subfolder,
+              newFolder: sanitizeSvc.sanitizeName(newfolder),
+              usePortalRoot: serviceConfig.usePortalRoot,
+              appId: appId
+            }
+          })
+          .then(svc.liveListReload);
+      };
 
-            // delete, then reload
-            // IF verb DELETE fails, so I'm using get for now
-            svc.delete = function (item) {
-                return $http.get(svc.url + '/delete',
-                        {
-                            params: {
-                                subfolder: svc.subfolder,
-                                isFolder: item.IsFolder,
-                                id: item.Id,
-                                usePortalRoot: serviceConfig.usePortalRoot,
-                                appId: appId
-                            }
-                        })
-                    .then(svc.liveListReload);
-            };
+      svc.goIntoFolder = function (childFolder) {
+        svc.folders.push(childFolder);
+        var pathParts = childFolder.Path.split('/');
+        var subPath = '';
+        for (var c = 0; c < svc.folders.length; c++)
+          subPath = pathParts[pathParts.length - c - 2] + '/' + subPath;
 
-            // rename, then reload
-            svc.rename = function (item, newName) {
-                return $http.get(svc.url + '/rename',
-                        {
-                            params: {
-                                subfolder: svc.subfolder,
-                                isFolder: item.IsFolder,
-                                id: item.Id,
-                                usePortalRoot: serviceConfig.usePortalRoot,
-                                newName: newName,
-                                appId: appId
-                            }
-                        })
-                    .then(svc.liveListReload);
-            };
-            
-            svc.reload = svc.liveListReload;
+        subPath = subPath.replace('//', '/');
+        if (subPath[subPath.length - 1] === '/')
+          subPath = subPath.substr(0, subPath.length - 1);
 
-            return svc;
-        };
-    });
+        // add configured Paths
+        subPath = (!!subfolder) ? subfolder + '/' + subPath : subPath;
+
+        childFolder.Subfolder = subPath;
+
+        // now assemble the correct subfolder based on the folders-array
+        svc.subfolder = subPath;
+        svc.liveListReload();
+        return subPath;
+      };
+
+      svc.goUp = function () {
+        if (svc.folders.length > 0)
+          svc.folders.pop();
+        if (svc.folders.length > 0) {
+          svc.subfolder = svc.folders[svc.folders.length - 1].Subfolder;
+        } else {
+          svc.subfolder = subfolder || '';
+        }
+        svc.liveListReload();
+        return svc.subfolder;
+      };
+
+      // delete, then reload
+      // IF verb DELETE fails, so I'm using get for now
+      svc.delete = function (item) {
+        return $http.get(svc.url + '/delete',
+          {
+            params: {
+              subfolder: svc.subfolder,
+              isFolder: item.IsFolder,
+              id: item.Id,
+              usePortalRoot: serviceConfig.usePortalRoot,
+              appId: appId
+            }
+          })
+          .then(svc.liveListReload);
+      };
+
+      // rename, then reload
+      svc.rename = function (item, newName) {
+        return $http.get(svc.url + '/rename',
+          {
+            params: {
+              subfolder: svc.subfolder,
+              isFolder: item.IsFolder,
+              id: item.Id,
+              usePortalRoot: serviceConfig.usePortalRoot,
+              newName: sanitizeSvc.sanitizeName(newName),
+              appId: appId
+            }
+          })
+          .then(svc.liveListReload);
+      };
+
+      svc.reload = svc.liveListReload;
+
+      return svc;
+    };
+  });
 // ReSharper restore PossiblyUnassignedProperty
