535184446a8be0ae2fc53b4544ecdcdc4650eeac
ijungleboy <daniel.mettler@2sic.com>
2020-01-22 16:08:28 +0100

    rebuild typings

--- typings/2sxc.api/$2/SxcRootV2.d.ts
+++
@@ -1,7 +0,0 @@
-import { Environment } from '../environment/Environment';
-import { SxcHttp } from './SxcHttp';
-export declare class SxcRootV2 {
-    env: Environment;
-    http: SxcHttp;
-    constructor();
-}
