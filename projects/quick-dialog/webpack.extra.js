9fcad4242281e6b481aba1c54ebc78f9c70278c0
ijungleboy <daniel.mettler@2sic.com>
2020-02-25 23:38:08 +0100

    source maps restored

--- projects/quick-dialog/webpack.extra.js
+++ projects/quick-dialog/webpack.extra.js
@@ -3,6 +3,6 @@ var setExternalSourceMaps = require('./build/external-source-maps');
 var configuration = {};
 
 // 2020-02-25 2dm temporarily disabled
-// setExternalSourceMaps(configuration, 'ng');
+setExternalSourceMaps(configuration, 'ng');
 
 module.exports = configuration;
