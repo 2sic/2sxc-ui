91e9e2a5ef7a0e09af07f3c8e1845fda5df0cfb6
ijungleboy <daniel.mettler@2sic.com>
2020-06-25 18:59:19 +0200

    create own project for snippets and simplified gulp

---
+++ projects/code-editor-snippets/gulpfile.js
@@ -0,0 +1,25 @@
+﻿(() => {
+    const
+        dest2sxc = 'C:/Projects/2sxc-dnn742/Website/DesktopModules/ToSIC_SexyContent/dist/ng-assets/',
+        gulp = require('gulp'),
+        jsXlsx = require('gulp-js-xlsx'),
+        rename = require('gulp-rename'),
+        // $ = require('gulp-load-plugins')({ lazy: false }),
+        rootDist = 'dist/';
+    
+    gulp.task('default', xlsxSnippetsToJson);
+
+    function xlsxSnippetsToJson() {
+        var src = 'src/snippets.xlsx';
+        return gulp.src(src)
+            .pipe(jsXlsx.run({
+                parseWorksheet: 'row_array'
+            }))
+            .pipe(rename({
+                extname: '.json.js'
+            }))
+            .pipe(gulp.dest(rootDist + 'code-snippets/'))
+            .pipe(gulp.dest(dest2sxc));
+    }
+
+})();
