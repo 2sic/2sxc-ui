6d47e00c90d752c7a47bdaf5c84ae67e76b1b2c0
ijungleboy <daniel.mettler@2sic.com>
2020-06-26 18:05:22 +0200

    include snippets in ci-auto-build

--- projects/code-editor-snippets/gulpfile.js
+++ projects/code-editor-snippets/gulpfile.js
@@ -1,24 +1,23 @@
-﻿(() => {
-    const
-        dest2sxc = 'C:/Projects/2sxc-dnn742/Website/DesktopModules/ToSIC_SexyContent/dist/ng-assets/',
-        gulp = require('gulp'),
-        jsXlsx = require('gulp-js-xlsx'),
-        rename = require('gulp-rename'),
-        rootDist = 'dist/';
-    
-    gulp.task('default', xlsxSnippetsToJson);
+﻿
+const
+    webpackHelpers = require('../webpack/webpack-helpers');
+    dest2sxc = webpackHelpers.DnnTargetFolder + 'dist/ng-assets/';
+    gulp = require('gulp'),
+    jsXlsx = require('gulp-js-xlsx'),
+    rename = require('gulp-rename'),
+    rootDist = 'dist/';
 
-    function xlsxSnippetsToJson() {
-        var src = 'snippets.xlsx';
-        return gulp.src(src)
-            .pipe(jsXlsx.run({
-                parseWorksheet: 'row_array'
-            }))
-            .pipe(rename({
-                extname: '.json.js'
-            }))
-            .pipe(gulp.dest(rootDist))
-            .pipe(gulp.dest(dest2sxc));
-    }
+gulp.task('default', xlsxSnippetsToJson);
 
-})();
+function xlsxSnippetsToJson() {
+    var src = 'snippets.xlsx';
+    return gulp.src(src)
+        .pipe(jsXlsx.run({
+            parseWorksheet: 'row_array'
+        }))
+        .pipe(rename({
+            extname: '.json.js'
+        }))
+        .pipe(gulp.dest(rootDist))
+        .pipe(gulp.dest(dest2sxc));
+}
