314ca77f77580ec04ceaf880fa67a45f658aaada
ijungleboy <daniel.mettler@2sic.com>
2020-08-30 18:10:06 +0200

    quick-dialog now building/distributing

--- projects/webpack/distribute-helpers2.js
+++ projects/webpack/distribute-helpers2.js
@@ -8,10 +8,7 @@ const debug = true;
 
 function showCopyProgress(e) {
   var now = new Date();
-  var time =
-    ("0" + now.getHours()).slice(-2)   + ":" +
-    ("0" + now.getMinutes()).slice(-2) + ":" +
-    ("0" + now.getSeconds()).slice(-2);
+  var time = `${(`0${now.getHours()}`).slice(-2)}:${(`0${now.getMinutes()}`).slice(-2)}:${(`0${now.getSeconds()}`).slice(-2)}`;
   console.log(chalk.green(`${time} copied : ${e.srcPath.padEnd(30, ' ')} -> ${e.dstPath}`));
 }
 
@@ -27,20 +24,19 @@ function startCpx(src, target, useWatch) {
 
 
 
-function createCallbackToRunAllCpx(cpxFilter, targetAddOn, watch) {
-  return () => {
+function runAllCpx(root, targetAddOn, watch) {
+    const cpxFilter = root + '/**/*.*';
     // Start CPX in copy or watch mode
     startCpx(cpxFilter, webpackHelpers.AssetsTarget + targetAddOn, watch);
     startCpx(cpxFilter, webpackHelpers.DnnTargetFolder + targetAddOn, watch);
-  }
 }
 
 // Watch the dist folder till the important folders are created, then start file sync
-function waitToWatchFolder(path, cpxFilter, targetAddOn) {
+function waitToRunAllCpx(path, targetAddOn) {
   const lastSlash = path.lastIndexOf('/');
   const parentPath = path.substr(0, lastSlash);
   const folderToWaitFor = path.substr(lastSlash + 1, 25);
-  console.log(chalk.blue(`Watch to Copy '${parentPath}', '${folderToWaitFor}', '${cpxFilter}', '${targetAddOn}'`));
+  console.log(chalk.blue(`Watch to Copy '${parentPath}', '${folderToWaitFor}', '${targetAddOn}'`));
 
 
   var runInnerCallback = true;
@@ -50,11 +46,11 @@ function waitToWatchFolder(path, cpxFilter, targetAddOn) {
         if(!runInnerCallback) return;
         if(debug) console.log(chalk.yellow(`checking '${path}'`));
         // If it's not the entry path we're looking for, just return
-        if(path.endsWith(`\\${folderToWaitFor}`) && path.endsWith(`/${folderToWaitFor}`)) return;
+        if(!path.endsWith(`\\${folderToWaitFor}`) && !path.endsWith(`/${folderToWaitFor}`)) return;
         console.log(chalk.blue(`Folder ${chalk.yellow(folderToWaitFor)} created, will stop parent-watcher`));
         // call the main cpx routine - but wait ca. 1 second as changes to the dist could break CPX
-        setTimeout(createCallbackToRunAllCpx(cpxFilter, targetAddOn, true), 1000);
-        // callback();
+        setTimeout(() => runAllCpx(path, targetAddOn, true), 1000);
+        // prevent further auto-starts for now
         runInnerCallback = false;
     })
     .on('unlinkDir', path => {
@@ -67,9 +63,9 @@ function waitToWatchFolder(path, cpxFilter, targetAddOn) {
         console.log(chalk.blue(`Folder ${chalk.yellow(folderToWaitFor)} was removed, will re-activate auto-start when re-created.`));
         runInnerCallback = true;
     });
+    console.log(chalk.blue(`Copy watchers running on '${parentPath}'`));
 }
 
 module.exports.showCopyProgress = showCopyProgress;
-// module.exports.startCpx = startCpx;
-module.exports.watchToCopy = waitToWatchFolder;
-module.exports.createCallbackToRunAllCpx = createCallbackToRunAllCpx;
+module.exports.waitToRunAllCpx = waitToRunAllCpx;
+module.exports.runAllCpx = runAllCpx;
--- projects/webpack/distribute2.js
+++ projects/webpack/distribute2.js
@@ -1,6 +1,5 @@
 
 const chalk = require('chalk');
-const webpackHelpers = require('./webpack-helpers.js');
 const cpxHelpers = require('./distribute-helpers2.js');
 
 // Get arguments from command line
@@ -10,22 +9,12 @@ console.log(chalk.blue(`Process running in ${process.cwd()} with args: `, args))
 if(args.length < 2) throw "Need src / dest parameters"
 let argId = 0;
 const root = args[argId++];
-// const part = args[argId++];
-const cpxSrc = root + '/**/*.*'; // args[argId++];
 const dst = args[argId++];
 const watch = args.length >= argId && args[argId++] === '-w';
 
 if(watch) 
-  cpxHelpers.watchToCopy(root, cpxSrc, dst); //, createCallbackToRunAllCpx(cpxSrc, dst, watch)); 
+  cpxHelpers.waitToRunAllCpx(root, dst);
 else
-  cpxHelpers.createCallbackToRunAllCpx(cpxSrc, dst, false)();
-
-// function createCallbackToRunAllCpx(cpxFilter, targetAddOn, watch) {
-//   return () => {
-//     // Start CPX in copy or watch mode
-//     cpxHelpers.startCpx(cpxFilter, webpackHelpers.AssetsTarget + targetAddOn, watch);
-//     cpxHelpers.startCpx(cpxFilter, webpackHelpers.DnnTargetFolder + targetAddOn, watch);
-//   }
-// }
+  cpxHelpers.runAllCpx(root, dst, false);
 
 console.log(chalk.blue('Copy watchers running...'));
