6b94e15b94ce8fb6224888a303a9542e1714d352
Petar Miličić <pagnodunadan@gmail.com>
2021-10-26 15:11:08 +0200

    Fixed null and undefined in NoJQ.param

--- projects/core/plumbing/no-jquery.ts
+++ projects/core/plumbing/no-jquery.ts
@@ -12,7 +12,7 @@ export class NoJQ {
     /** https://api.jquery.com/jquery.param/ */
     static param(obj: any): string {
         const param = Object.entries(obj)
-            .map(([k, v]) => `${encodeURIComponent(k)}=${typeof v === 'string' ? encodeURIComponent(v) : encodeURIComponent(JSON.stringify(v))}`)
+            .map(([k, v]) => `${encodeURIComponent(k)}=${v == null ? '' : typeof v === 'string' ? encodeURIComponent(v) : encodeURIComponent(JSON.stringify(v))}`)
             .join('&');
         return param;
     }
