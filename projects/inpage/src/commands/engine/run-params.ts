d020fa466336aa1bcd604b6a879df199f6dec476
ijungleboy <daniel.mettler@2sic.com>
2021-11-12 18:50:24 +0100

    minor

--- projects/inpage/src/commands/engine/run-params.ts
+++ projects/inpage/src/commands/engine/run-params.ts
@@ -2,30 +2,31 @@ import { WorkflowStep } from '../../workflow/workflow-step';
 import { CommandParams } from '../command-params';
 
 /**
- * WIP v12.10
- * Should extend the existing Command Params and at the same time ...?
+ * Parameters for the cms.run(...) command.
+ * New in 12.10
  * @export
  * @interface RunParams
  * @extends {CommandParams}
  */
-export interface RunParams /* extends CommandParams */ {
+export interface RunParams {
 
     /** The tag on which the run was triggered - it's used to give the command a context to start from */
     tag: HTMLElement;
 
-    /** The action to perform, always required */
-    action: string;
+    /** The action to perform. Required if you don't have params which themselves have the action */
+    action?: string;
 
-    /** todo */
+    /** The command params, like contentType, entityId etc. */
     params?: CommandParams;
 
-    /** The run command can also carry the event which triggered it (like the click) for further use */
+    /** The event which triggered this command - sometimes useful internally further use */
     event?: MouseEvent;
 
-    /** WIP v12.10 Workflow step would enable a command to also include workflow actions */
-    workflowSteps: WorkflowStep | WorkflowStep[];
+    /** Workflow steps work the same way as with a toolbar */
+    workflowSteps?: WorkflowStep | WorkflowStep[];
 }
 
 export function isRunParams(maybeRunParams: unknown): maybeRunParams is RunParams {
-    return (maybeRunParams as RunParams).tag != null && (maybeRunParams as RunParams).action != null;
+    const typed = maybeRunParams as RunParams;
+    return typed.tag != null && (typed.action != null || typed.params != null);
 }
