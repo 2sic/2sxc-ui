9242af820b603c8d67041fcb3439ee3f5efe1770
Petar Miličić <pagnodunadan@gmail.com>
2022-02-14 10:15:29 +0100

    Typings build process and configuration

--- projects/$2sxc/webpack.config.js
+++ projects/$2sxc/webpack.config.js
@@ -4,37 +4,51 @@ const webpackHelpers = require('../webpack/webpack-helpers.js');
 const dnnTarget = webpackHelpers.DnnTargetFolder + 'js';
 const assetsTarget = webpackHelpers.AssetsTarget + 'js';
 
-const configuration = {
+function buildConfiguration(env, argv) {
+  const configuration = {
     mode: 'development',
-    entry: "./src/2sxc.api.ts",
+    entry: './src/2sxc.api.ts',
     devtool: 'source-map',
     module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              configFile: argv.onlyTypings ? 'tsconfig.typings.json' : 'tsconfig.json'
             }
-        ]
+          },
+          exclude: /node_modules/
+        }
+      ]
     },
     resolve: {
-        extensions: [".tsx", ".ts", ".js"]
+      extensions: ['.tsx', '.ts', '.js']
     },
     output: {
-        filename: "2sxc.api" + ".min.js",
-        path: assetsTarget,
-        library: '$2sxcJsApi',
+      filename: '2sxc.api' + '.min.js',
+      path: assetsTarget,
+      library: '$2sxcJsApi',
     },
     plugins: [
-        webpackHelpers.CreateDefinePlugin(webpack),
-        // after build, copy the files from the project assets
-        webpackHelpers.createCopyAfterBuildPlugin(assetsTarget, /* dnnTarget */ webpackHelpers.TargetsWithoutAssets, 'js'),
+      webpackHelpers.CreateDefinePlugin(webpack),
+      ...(
+        argv.onlyTypings
+          ? []
+          // after build, copy the files from the project assets
+          : [webpackHelpers.createCopyAfterBuildPlugin(assetsTarget, /* dnnTarget */ webpackHelpers.TargetsWithoutAssets, 'js')]
+      )
     ]
-};
-
-/* change source map generation based on production mode */
+  };
+  return configuration;
+}
 
 module.exports = (env, argv) => {
+  const configuration = buildConfiguration(env, argv);
+  if (!argv.onlyTypings) {
+    // change source map generation based on production mode
     webpackHelpers.SetExternalSourceMaps(webpack, argv.mode, configuration, 'js');
-    return configuration;
+  }
+  return configuration;
 }
