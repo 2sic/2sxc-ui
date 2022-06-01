0229ed4ccabf234cf65196be9f6bfba3a32f49be
ijungleboy <daniel.mettler@2sic.com>
2022-06-01 16:49:39 +0200

    wip building complex types

---
+++ projects/sxc-typings/dist/inpage/src/context/bundles/index.d.ts
@@ -0,0 +1,4 @@
+export * from './context-bundle-button';
+export * from './context-bundle-instance';
+export * from './context-bundle-content';
+export * from './context-bundle-toolbar';
---
+++ projects/sxc-typings/dist/inpage/src/workflow/index.d.ts
@@ -0,0 +1,6 @@
+export * from './workflow-manager';
+export * from './workflow-arguments';
+export * from './workflow-phases';
+export * from './workflow';
+export * from './workflow-helper';
+export * from './toolbar-with-workflow';
