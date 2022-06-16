aac6985c73758cb1ffef808a1e630fb6f30077a9
ijungleboy <daniel.mettler@2sic.com>
2022-06-16 10:43:34 +0200

    reduce params passed in url to dialogs #2788

--- projects/inpage/src/manage/ng-dialog-params.ts
+++ projects/inpage/src/manage/ng-dialog-params.ts
@@ -9,65 +9,59 @@
 export class NgUrlValuesWithoutParams {
   readonly zoneId: number;
   readonly appId: number;
-  readonly tid: number;
+  // #reduceEnvVars
+  // readonly tid: number;
   readonly mid: number;
   readonly cbid: number;
 
-  // 2021-01-19
-  // readonly lang: string;
-  // readonly langpri: string;
-  // readonly langs: string;
-  // 2020-11-28 #cleanup11.11 2dm - not used, disabled - keep till Jan 2021, then remove from backend-json and drop these comments
-  // readonly portalroot: string;
-  // 2020-12-11 #cleanup11.11 2dm - doesn't seem used, disabled
-  // readonly websiteroot: string;
   readonly partOfPage?: boolean;
   readonly publishing?: string;
-  // 2021-01-19
-  // readonly user: ContextOfUser;
-  readonly approot: string | null;
+  // #reduceEnvVars
+  // readonly approot: string | null;
 
+  // #reduceEnvVars
   /** The api root for all 2sxc API calls - will vary by platform */
-  readonly api: string;
+  // readonly api: string;
 
-  /** The root path to the UIs (scripts, css etc.) */
-  // 2021-01-19
-  // readonly uiRoot: string;
-
-  /** new in 10.27 */
+  /** new in 10.27 - for quick-dialog */
   readonly apps?: string;
 
+  // 2022-06-16 2dm disabled, believe it's not used any more
   /** features of App - this is to tell the UI it can show advanced features of an app like permissions, API, REST etc. */
-  readonly fa: boolean;
+  // readonly fa: boolean;
 
+  // #reduceEnvVars
   /** request verification token header name */
-  readonly rvth: string;
+  // readonly rvth: string;
 
+  // #reduceEnvVars
   /** request verification token value */
-  readonly rvt: string;
+  // readonly rvt: string;
 
   constructor(context: ContextComplete, partOfPage: boolean) {
 
     const $2sxc = window.$2sxc;
     // #CustomContext
     const ctx = context.sxc?.ctx;
-    const ctxAny = ctx as any;
-    if (ctxAny?.complete === true) {
+    if (ctx?.complete === true) {
         this.zoneId = ctx.zoneId;
         this.appId = ctx.appId;
-        if (ctx.pageId != null) this.tid = ctx.pageId;
+        // #reduceEnvVars
+        // if (ctx.pageId != null) this.tid = ctx.pageId;
         if (ctx.moduleId != null) this.mid = ctx.moduleId;
-        const cbid = ctxAny?.blockId ?? this.mid;
-        if (cbid != null && cbid !== '') this.cbid = cbid;
+        const cbid = ctx?.blockId ?? this.mid;
+        if (cbid != null && cbid) this.cbid = cbid;
     } else {
         this.zoneId = ctx?.zoneId ?? context.app.zoneId;
         this.appId = ctx?.appId ?? context.app.id;
-        this.tid = ctx?.pageId ?? context.page.id;
+        // #reduceEnvVars
+        // this.tid = ctx?.pageId ?? context.page.id;
         this.mid = ctx?.moduleId ?? context.instance.id;
-        this.cbid = ctxAny?.blockId ?? context.contentBlockReference.id;
+        this.cbid = ctx?.blockId ?? context.contentBlockReference.id;
     }
 
-    this.api = $2sxc.env.api();
+    // #reduceEnvVars
+    // this.api = $2sxc.env.api();
 
     this.partOfPage = partOfPage;
     if (partOfPage) this.publishing = context.contentBlockReference.publishingMode;
@@ -75,12 +69,16 @@ export class NgUrlValuesWithoutParams {
     // 2021-01-19 2dm - should now not be used any more
     // this.user = ContextOfUser.fromContext(context);
     // TODO: #paramCleanUp - approot is probably not used anymore. verify and remove
-    this.approot = context.app.appPath || null; // this is the only value which doesn't have a slash by default. note that the app-root doesn't exist when opening "manage-app"
+
+    // #reduceEnvVars
+    // this.approot = context.app.appPath || null; // this is the only value which doesn't have a slash by default. note that the app-root doesn't exist when opening "manage-app"
     if (context?.button?.command?.params?.apps)
         this.apps = context.button.command.params.apps;
+
     // TODO: #paramCleanUp - fa is probably not used anymore. verify and remove
-    this.fa = !context.app.isContent;
-    this.rvth = $2sxc.env.rvtHeader();
-    this.rvt = $2sxc.env.rvt();
+    // #reduceEnvVars
+    // this.fa = !context.app.isContent;
+    // this.rvth = $2sxc.env.rvtHeader();
+    // this.rvt = $2sxc.env.rvt();
   }
 }
