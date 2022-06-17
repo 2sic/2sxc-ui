b650b7cb3c26c5f46baf574908031d047a9a2161
ijungleboy <daniel.mettler@2sic.com>
2022-06-17 08:57:22 +0200

    file renames

---
+++ projects/inpage/src/commands/command/command-index.ts
@@ -0,0 +1,40 @@
+// TODO
+
+// tslint:disable: align
+
+/** @internal */ export * from './shared-logic';
+/** @internal */ import './command-add';
+/** @internal */ import './command-add-existing';
+/** @internal */ import './command-app';
+/** @internal */ import './command-app-import';
+/** @internal */ import './command-app-resources';
+/** @internal */ import './command-app-settings';
+/** @internal */ import './command-apps';
+/** @internal */ import './command-content-items';
+/** @internal */ import './command-content-type';
+/** @internal */ import './command-copy';
+/** @internal */ import './command-custom';
+/** @internal */ import './command-delete';
+/** @internal */ import './command-edit';
+/** @internal */ import './command-insights-server';
+/** @internal */ import './command-instance-list';
+/** @internal */ import './command-layout';
+/** @internal */ import './command-metadata';
+/** @internal */ import './command-more';
+/** @internal */ import './command-movedown';
+/** @internal */ import './command-moveup';
+/** @internal */ import './command-new';
+/** @internal */ import './command-publish';
+/** @internal */ import './command-remove';
+/** @internal */ import './command-replace';
+/** @internal */ import './command-template-develop';
+/** @internal */ import './command-template-query';
+/** @internal */ import './command-template-settings';
+/** @internal */ import './command-zone';
+
+
+/** @internal */ export * from './content-list-action-params';
+/** @internal */ export * from './content-list-actions';
+
+// Workflow
+/** @internal */ export * from './workflow-commands';
--- projects/inpage/src/commands/index.ts
+++ projects/inpage/src/commands/index.ts
@@ -3,7 +3,7 @@ export * from './command-names';
 export * from './command-code';
 export * from './command';
 export * from './commands';
-export * from './command/index';
+export * from './command/command-index';
 export * from '../../../$2sxc/src/cms/command-params';
 export * from './engine/cms-engine';
 export * from './command-code';
