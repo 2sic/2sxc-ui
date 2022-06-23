d60a0011dd981045cc98014056f3a3b4d3098066
ijungleboy <daniel.mettler@2sic.com>
2022-06-23 18:45:43 +0200

    fix generate types

---
+++ projects/sxc-typings/parts/index.d.ts
@@ -0,0 +1,7 @@
+// type definitions for 2sxc
+// project: 2sxc typings
+// definitions by: Daniel Mettler, 2sic <https://2sxc.org/>
+
+export * from './dist/$2sxc/src';
+
+export * from './dist/core';
