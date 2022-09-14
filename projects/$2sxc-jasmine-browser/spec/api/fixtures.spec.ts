c18968049db25d33a3c44c7de5cd1aebaeb6a31f
Tonći Vatavuk <tonci@sistemi.hr>
2022-09-14 13:02:54 +0200

    clean

--- projects/$2sxc-jasmine-browser/spec/api/fixtures.spec.ts
+++ projects/$2sxc-jasmine-browser/spec/api/fixtures.spec.ts
@@ -9,9 +9,7 @@ describe('2sxc.api html fixtures', () => {
 
     beforeEach(function () {
       fixture = $("<div class='sc-content-block'  data-cb-instance='123' data-cb-id='345'>mock</div>");
-      console.log("fixture", fixture[0]);
       sxc = $2sxc(fixture[0]);
-      console.log("sxc", sxc);
     });
 
     it("isEditMode()=false", function () {
@@ -32,13 +30,12 @@ describe('2sxc.api html fixtures', () => {
   describe('2sxc.api data-edit-context html fixtures', () => {
 
     beforeEach(function () {
-      fixture = $(`<div class='sc-content-block'  data-cb-instance='385' data-cb-id='385'
-      data-edit-context='{"Environment":{"WebsiteId":0,"WebsiteUrl":"//dnn9102-farm2.dnndev.me/","PageId":35,"PageUrl":"http://dnn9102-farm2.dnndev.me/Redis","parameters":[],"InstanceId":385,"SxcVersion":"14.7.5.1662979741","SxcRootUrl":"/","IsEditable":true},"User":{"CanDevelop":true,"CanAdmin":true},"Language":{"Current":"en-us","Primary":"en-us","All":[]},"contentBlockReference":{"publishingMode":"DraftOptional","id":385,"parentIndex":0,"partOfPage":true},"contentBlock":{"IsCreated":true,"IsList":false,"TemplateId":2568,"QueryId":null,"ContentTypeName":"0c67f102-307a-4743-9575-fb1d14d79a8e","AppUrl":"/Portals/0/2sxc/acache","AppSharedUrl":"/Portals/_default/2sxc/acache","AppSettingsId":null,"AppResourcesId":null,"IsContent":false,"HasContent":true,"SupportsAjax":false,"TemplatePath":"/_v1.cshtml","TemplateIsShared":false,"ZoneId":2,"AppId":27,"Guid":"243f3f38-a350-4964-9cee-44e05148bbf3","Id":2570},"error":{"type":null,"Message":null},"Ui":{"AutoToolbar":true}}'>
-    `);
-      console.log("fixture", fixture[0]);
-
+      fixture = $(`<div class='sc-content-block'
+        data-cb-instance='385'
+        data-cb-id='385'
+        data-edit-context='{"Environment":{"WebsiteId":0,"WebsiteUrl":"//dnn9102-farm2.dnndev.me/","PageId":35,"PageUrl":"http://dnn9102-farm2.dnndev.me/Redis","parameters":[],"InstanceId":385,"SxcVersion":"14.7.5.1662979741","SxcRootUrl":"/","IsEditable":true},"User":{"CanDevelop":true,"CanAdmin":true},"Language":{"Current":"en-us","Primary":"en-us","All":[]},"contentBlockReference":{"publishingMode":"DraftOptional","id":385,"parentIndex":0,"partOfPage":true},"contentBlock":{"IsCreated":true,"IsList":false,"TemplateId":2568,"QueryId":null,"ContentTypeName":"0c67f102-307a-4743-9575-fb1d14d79a8e","AppUrl":"/Portals/0/2sxc/acache","AppSharedUrl":"/Portals/_default/2sxc/acache","AppSettingsId":null,"AppResourcesId":null,"IsContent":false,"HasContent":true,"SupportsAjax":false,"TemplatePath":"/_v1.cshtml","TemplateIsShared":false,"ZoneId":2,"AppId":27,"Guid":"243f3f38-a350-4964-9cee-44e05148bbf3","Id":2570},"error":{"type":null,"Message":null},"Ui":{"AutoToolbar":true}}'
+        >`);
       sxc = $2sxc(fixture[0]);
-      console.log("sxc", sxc);
     });
 
     it("isEditMode()=false", function () {
@@ -54,8 +51,6 @@ describe('2sxc.api html fixtures', () => {
     });
 
     // xit("data-edit-context", function () {
-    //   expect(sxc.id).toBe(385);
-    //   expect(sxc.cbid).toBe(345);
     //   const env = sxc.webApi.env;
     //   console.log("env", env);
     // });
