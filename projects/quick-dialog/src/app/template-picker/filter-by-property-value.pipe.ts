57bf13438bae24ed1985630c303873050c52b2b6
ijungleboy <daniel.mettler@2sic.com>
2022-11-04 19:04:08 +0100

    fix issue where content-type appeared without views

--- projects/quick-dialog/src/app/template-picker/filter-by-property-value.pipe.ts
+++ projects/quick-dialog/src/app/template-picker/filter-by-property-value.pipe.ts
@@ -6,3 +6,12 @@ export class FilterByPropertyValuePipe implements PipeTransform {
     return input?.filter(i => i[propertyName]?.toLocaleLowerCase?.().includes?.(filterValue?.toLocaleLowerCase?.())) ?? input;
   }
 }
+
+
+@Pipe({ name: 'filterByBoolProperty' })
+export class FilterByBoolPropertyPipe implements PipeTransform {
+  transform<T>(input: T[], propertyName: string, filterValue: boolean): T[] {
+    // console.log('debug 2dm', input);
+    return input?.filter(i => i[propertyName] === filterValue) ?? input;
+  }
+}
