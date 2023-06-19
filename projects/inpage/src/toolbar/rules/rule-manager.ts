4caaf8a97d83bf292e17c18bae0c44eff235516b
ijungleboy <daniel.mettler@2sic.com>
2023-06-19 07:41:20 +0200

    improve buttons on obsolete warnings https://github.com/2sic/2sxc/issues/3110

--- projects/inpage/src/toolbar/rules/rule-manager.ts
+++ projects/inpage/src/toolbar/rules/rule-manager.ts
@@ -1,5 +1,6 @@
 import { Operations as OP, BuildRule } from '.';
 import { CommandNames } from '../../commands';
+import { insightsUrl } from '../../commands/command/command-insights';
 import { Debug } from '../../constants/debug';
 import { ContextComplete } from '../../context';
 import { HasLog } from '../../core';
@@ -12,17 +13,15 @@ const debug = Debug.parts.RuleManager;
 const throwOnError = true;
 const devInfoButtonsIndex = 20;
 
-const buttonRip: ToolbarButtonSettings & Partial<ToolbarSettings> = {
+const ripButton = { // ToolbarButtonSettings & Partial<ToolbarSettings> = {
     color: 'orange',
     icon: '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M368 432V192c0-79.5-64.5-144-144-144S80 112.5 80 192V432H32V192C32 86 118 0 224 0S416 86 416 192V432H368zM0 488c0-13.3 10.7-24 24-24H424c13.3 0 24 10.7 24 24s-10.7 24-24 24H24c-13.3 0-24-10.7-24-24zM248 152v40h48c13.3 0 24 10.7 24 24s-10.7 24-24 24H248V360c0 13.3-10.7 24-24 24s-24-10.7-24-24V240H152c-13.3 0-24-10.7-24-24s10.7-24 24-24h48V152c0-13.3 10.7-24 24-24s24 10.7 24 24z"/></svg>',
     note: {
       note: `<strong>Obsolete Code detected</strong>
       <br>
-      This uses obsolete code which will be removed soon. You should fix the code so it won't break on a future update.
-      <br>
-      Click on the button to see all obsolete code.`,
+      This uses obsolete code which will be removed soon. You should fix the code so it won't break on a future update.`,
       allowHtml: true,
-    }
+    },
 }
 
 /**
@@ -91,7 +90,24 @@ export class RuleManager extends HasLog {
     if (problems.find(p => p.code === codeObsolete) !== undefined) {
       const insightsRule = BuildRule.Create({
         name: CommandNames.insights,
-        ui: buttonRip,
+        ui: {
+          ...ripButton,
+          note: {
+            links: [
+              { 
+                url: insightsUrl(`logs?key=warnings-obsolete&filter=AppId=${context.app.id}`),
+                label: 'Obsolete this App',
+                primary: true
+              },
+              { 
+                url: insightsUrl('logs?key=warnings-obsolete'),
+                label: 'all obsolete',
+                primary: false
+              }
+            ],
+            ...ripButton.note
+          }
+        },
         params: { part: 'Logs?&key=warnings-obsolete'},
         pos: 11,
         log: this.log
