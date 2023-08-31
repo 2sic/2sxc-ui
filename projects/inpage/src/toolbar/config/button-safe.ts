4f37b112e884ce8f48ef1d015cbe7123eb4d8a7a
ijungleboy <daniel.mettler@2sic.com>
2023-08-31 19:08:48 +0200

    improve image toolbar label

--- projects/inpage/src/toolbar/config/button-safe.ts
+++ projects/inpage/src/toolbar/config/button-safe.ts
@@ -19,47 +19,45 @@ export class ButtonSafe {
      * New v15.04 for `info`
      * @returns the color of the button, or undefined if not set
      */
-    color = () => getVal (this.button.color, this.context, undefined);
+    color = () => getVal(this.button.color, this.context, undefined);
 
     /** The dialog name - should default to the name */
-    dialog = () => getVal (this.button.dialog, this.context, this.button.command.name);
+    dialog = () => getVal(this.button.dialog, this.context, this.button.command.name);
 
     /** Determines if the button should be disabled */
-    disabled = () => getVal (this.button.disabled, this.context, false);
+    disabled = () => getVal(this.button.disabled, this.context, false);
 
     /** Dynamically determine classes - must always be a function */
-    dynamicClasses = () => getVal (this.button.dynamicClasses, this.context, '');
+    dynamicClasses = () => getVal(this.button.dynamicClasses, this.context, '');
 
     /** Check if full-screen, always a function */
-    fullScreen = () => getVal (this.button.fullScreen, this.context, false);
+    fullScreen = () => getVal(this.button.fullScreen, this.context, false);
 
     /** The icon to show in the button */
-    icon = () => getVal (this.button.icon, this.context, '');
+    icon = () => getVal(this.button.icon, this.context, '');
 
     /** Determine if it should use the inline window, always a function */
-    inlineWindow = () => getVal (this.button.inlineWindow, this.context, false);
+    inlineWindow = () => getVal(this.button.inlineWindow, this.context, false);
 
     /** Check if we should open a new window, always an FN */
-    newWindow = () => getVal (this.button.newWindow, this.context, false);
+    newWindow = () => getVal(this.button.newWindow, this.context, false);
 
     /** The parameters which are used to run the command */
-    addParamsToLink = () => getVal (this.button.addParamsToLink, this.context, {});
-
+    addParamsToLink = () => getVal(this.button.addParamsToLink, this.context, {});
 
     /** Determines if this button runs in the page - affecting publishing */
-    partOfPage = () => getVal (this.button.partOfPage, this.context, false);
-
+    partOfPage = () => getVal(this.button.partOfPage, this.context, false);
 
     /** Method which determines if it should be shown or not */
-    showCondition = () => getVal (this.button.showCondition, this.context, true);
+    showCondition = () => getVal(this.button.showCondition, this.context, true);
 
     tippy = (context: ContextComplete, tag: HTMLElement) => this.button.tippy?.(context, tag);
 
     /** The title of this button which will usually be i18n keys */
-    title = () => getVal (this.button.title, this.context, 'unknown-title');
+    title = () => getVal(this.button.title, this.context, 'unknown-title');
 
     /** this is just a UI interaction, won't create data so won't need pre-flight */
-    uiActionOnly = () => getVal (this.button.uiActionOnly, this.context, true);
+    uiActionOnly = () => getVal(this.button.uiActionOnly, this.context, true);
 
 }
 
