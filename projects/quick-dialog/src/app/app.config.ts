9748fd7c9cae99e6f608045d7d96297dca205d2b
paulpietzko <paul.pietzko@icloud.com>
2025-04-29 09:31:35 +0200

    feat: implement custom translation loader to handle missing files gracefully

--- projects/quick-dialog/src/app/app.config.ts
+++ projects/quick-dialog/src/app/app.config.ts
@@ -5,10 +5,27 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
-import { i18nExtension, pathToI18n } from "./i18n";
+import { i18nExtension, pathToI18n, PrimaryUiLanguage } from "./i18n";
+import { catchError } from "rxjs/operators";
+import { of } from "rxjs";
+
+// 2pp: Implemented a custom loader to handle missing translation files gracefully
+// Otherwise, the app would show: HttpErrorResponse {headers: HttpHeaders, status: 404, statusText: 'Not Found',
+// Docs don't mention this: https://ngx-translate.org/recipes/handle-missing-translations/
+// But it seems to be a common issue with the library. See: https://github.com/ngx-translate/core/issues/1002
+export class CustomTranslateLoader extends TranslateHttpLoader {
+  override getTranslation(lang: string) {
+    return super.getTranslation(lang).pipe(
+      catchError(() => {
+        console.warn(`Translation file for '${lang}' not found. Using fallback '${PrimaryUiLanguage}'.`);
+        return of({});
+      })
+    );
+  }
+}
 
 export function HttpLoaderFactory(http: HttpClient) {
-  const loader = new TranslateHttpLoader(http, pathToI18n, i18nExtension);
+  const loader = new CustomTranslateLoader(http, pathToI18n, i18nExtension);
   // log.add('created translate-loader', loader);
   return loader;
 }
