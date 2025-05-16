cf4a3430bda621aa1baf12f12b37734d74cc842d
paulpietzko <paul.pietzko@icloud.com>
2025-05-16 15:32:52 +0200

    feat: configure karma for typescipt

--- projects/$2sxc/karma.conf.js
+++ projects/$2sxc/karma.conf.js
@@ -1,68 +1,41 @@
 // Karma configuration
-// Generated on Fri May 16 2025 15:25:08 GMT+0200 (Mitteleuropäische Sommerzeit)
+// Generated on Fri May 16 2025 13:34:01 GMT+0200 (Mitteleuropäische Sommerzeit)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
+    basePath: "",
+    files: ["./test/*.ts"],
+    exclude: ["./test/assests/*"],
+    frameworks: ["jasmine", "karma-typescript"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-typescript"),
     ],
-
-
-    // list of files / patterns to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
-    preprocessors: {
+    client: {
+      clearContext: false,
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser instances should be started simultaneously
-    concurrency: Infinity
-  })
-}
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    reporters: ["progress", "kjhtml"],
+    browsers: ["Chrome"],
+    restartOnFileChange: true,
+    preprocessors: {
+      "./test/*.ts": ["karma-typescript"],
+    },
+    karmaTypescriptConfig: {
+      tsconfig: "./tsconfig.json",
+      compilerOptions: {
+        lib: ["es2021", "dom"]
+      },
+    },
+  });
+};
