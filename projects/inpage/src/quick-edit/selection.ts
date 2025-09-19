e0fe85fe3e82ee6fc17fc7ccb72dba62d2207092
Paul Pietzko <paul.pietzko@icloud.com>
2025-09-19 13:30:58 +0200

    feat: format quick-e files with prettier (no logic changes)

--- projects/inpage/src/quick-edit/selection.ts
+++ projects/inpage/src/quick-edit/selection.ts
@@ -2,17 +2,17 @@
  * @internal
  */
 export class Selection {
-    /** The parent is either "dnn" or a module information */
-    parent: string | number;
+  /** The parent is either "dnn" or a module information */
+  parent: string | number;
 
-    // new in 11.06 - inner content now always uses guid
-    parentGuid: string;
+  // new in 11.06 - inner content now always uses guid
+  parentGuid: string;
 
-    /** The field inside the parent used for this content */
-    field: string;
+  /** The field inside the parent used for this content */
+  field: string;
 
-    list: HTMLElement;
-    item: HTMLElement;
-    index: number;
-    type: 'mod' | 'cb';
+  list: HTMLElement;
+  item: HTMLElement;
+  index: number;
+  type: "mod" | "cb";
 }
