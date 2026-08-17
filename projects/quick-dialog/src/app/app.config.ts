fd461eb01c1e43e9966e0d6e54c013e813bf1190
giovi <npro9021@gmail.com>
2026-08-17 12:00:02 +0200

    update packages

--- projects/quick-dialog/src/app/app.config.ts
+++ projects/quick-dialog/src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { SxcHttpInterceptorProvider } from "@2sic.com/sxc-angular";
 import { HttpClient, withInterceptorsFromDi, provideHttpClient } from "@angular/common/http";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { TranslateModule, TranslateLoader, TranslateModuleConfig } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { i18nExtension, pathToI18n, PrimaryUiLanguage } from "./i18n";
@@ -35,8 +33,6 @@ export const appConfig: ApplicationConfig = {
     SxcHttpInterceptorProvider,
     provideHttpClient(withInterceptorsFromDi()),
     importProvidersFrom(
-      BrowserModule,
-      BrowserAnimationsModule,
       TranslateModule.forRoot(),
       TranslateModule.forChild(buildTranslateConfiguration(HttpLoaderFactory)),
     ),
