7e75dd0cb0ef079cbbfb51a7610cc509359ad12a
giovi <npro9021@gmail.com>
2026-08-17 12:00:02 +0200

    update packages

--- projects/quick-dialog/src/main.ts
+++ projects/quick-dialog/src/main.ts
@@ -1,6 +1,5 @@
 import { startWith } from "rxjs/operators";
-import { enableProdMode, provideZoneChangeDetection } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { destroyPlatform, enableProdMode, provideZoneChangeDetection } from "@angular/core";
 import { environment } from "./environments/environment";
 import { BootController } from "./app/core/boot-control";
 import { log } from "app/core/log";
@@ -15,14 +14,13 @@ if (environment.production) {
 log.add("loading main.ts");
 
 declare const window;
-const platform = platformBrowserDynamic();
 
 function init() {
   log.add("init()");
 
   try {
     // kill listeners
-    if (!platform.destroyed) platform.destroy();
+    destroyPlatform();
   } catch (e) {
     console.log("platform destroy error", e);
   }
