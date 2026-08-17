ab58e7ea89ce60dc17aaeef07c883dd638881aa6
giovi <npro9021@gmail.com>
2026-08-17 12:00:02 +0200

    update packages

--- projects/quick-dialog/src/polyfills.ts
+++ projects/quick-dialog/src/polyfills.ts
@@ -34,10 +34,6 @@
 // import 'core-js/es/set';
 
 
-/** Evergreen browsers require these. **/
-import 'core-js/es/reflect';
-
-
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
