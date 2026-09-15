df0ecb82aa9767b88bbca8032d41a4a32d89f6f5
giovi <npro9021@gmail.com>
2026-09-15 10:49:50 +0200

    minor

--- projects/sxc-angular/projects/sxc-angular/src/lib/beta/edit.ts
+++ projects/sxc-angular/projects/sxc-angular/src/lib/beta/edit.ts
@@ -6,11 +6,16 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 })
 export class SxcToolbarDirective implements OnInit {
   @Input() config: any = {};
-  constructor(private elementRef: ElementRef, private context: Context) {}
+  
+  constructor(
+    private elementRef: ElementRef,
+    private context: Context
+  ) {}
 
   ngOnInit() {
     const sxc = this.context.sxc as any;
-    if (!sxc.manage) return; // edit not available, probably not logged in
+    if (!sxc.manage)
+      return; // edit not available, probably not logged in
     this.elementRef.nativeElement.innerHTML = sxc.manage.getToolbar(this.config.toolbar, this.config.settings);
   }
 
