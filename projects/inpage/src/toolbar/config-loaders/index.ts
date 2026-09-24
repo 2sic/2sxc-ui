cc55b8cd93b934cbcac9d7b0fd75ca01f4adf4ae
iJungleboy <daniel.mettler@2sic.com>
2026-09-24 09:20:43 +0200

    minor fix inpage

--- projects/inpage/src/toolbar/config-loaders/index.ts
+++ projects/inpage/src/toolbar/config-loaders/index.ts
@@ -4,7 +4,7 @@ export * from './config-formats/in-page-command';
 export * from './config-formats/in-page-button-group';
 
 export * from './button-config-loader';
-export * from './command-config-loader';
+// export * from './command-config-loader';
 export * from './group-config-loader';
 
 export * from './toolbar-config-loader-v09';
